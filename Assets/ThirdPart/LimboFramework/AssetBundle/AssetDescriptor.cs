6ab0002c083bfd53adaa23b5c4c368f462e2f68f
Rgbvr <hanxusheng_ok@163.comn>
2018-10-30 20:34:36 +0800

    no message

--- Assets/ThirdPart/LimboFramework/AssetBundle/AssetDescriptor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LimboFramework.AssetBundles
-{
-	public class AssetDescriptor
-    {
-        public int HashCode { get; set; }
-        public string Name { get; set; }
-        public long Size { get; set; }
-    }
-}
